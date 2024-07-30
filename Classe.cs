0cae879a1fad8cf0a01b41b2434a2b04bd073c83
Cindy T <cindy.trinh@hotmail.fr>
2024-07-30 11:38:42 +0200

    Ajoutez des fichiers projet.

---
+++ Classe.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+/// <summary>
+/// Tableaux pour stocker les élèves et les matières enseignées dans la classe
+/// </summary>
+namespace TPMoyennes
+{
+	class Classe
+	{
+		// Déclaration des variables
+		public string nomClasse { get; private set; }
+		public List<Eleve> eleves { get; private set; } = new List<Eleve>();
+		public List<string> matieres { get; private set; } = new List<string>();
+		public int nbrEleves { get; private set; }
+        public int nbrMatieres { get; private set; }
+        public float moyenne { get; private set; }
+        public List<float> listMoyenneMat { get; private set; } = new List<float>();
+        public List<float> listMoyenneGen { get; private set; } = new List<float>();
+
+        // Constructeur
+        public Classe(string aNomClasse)
+		{
+			nomClasse = aNomClasse;
+			nbrEleves = 0;
+            nbrMatieres = 0;
+ 		}
+
+		// Remplissage du tableau des élèves dans la classe
+		public void ajouterEleve(string prenomEleve, string nomEleve)
+		{			
+			Eleve nouvEleve = new Eleve(prenomEleve, nomEleve, nbrEleves);
+			eleves.Add(nouvEleve);
+			nbrEleves++;	
+		}
+
+		// Remplissage du tableau des matières enseignées dans la classe
+		public void ajouterMatiere(string matiere)
+		{
+			matieres.Add(matiere);
+            nbrMatieres++;
+		}
+
+        // Calcul de la moyenne de la classe dans une matière
+        public float moyenneMatiere(int numMatiere)
+        {
+            listMoyenneMat.Clear();
+            for (int iele = 0; iele < nbrEleves; iele++)
+            {
+                listMoyenneMat.Add(eleves[iele].moyenneMatiere(numMatiere));
+            }
+            moyenne = listMoyenneMat.Average();
+			moyenne = (float)Math.Round(moyenne, 2);
+            return moyenne;
+
+        }
+
+        // Calcul de la moyenne générale de la classe
+        public float moyenneGeneral()
+        {
+            listMoyenneGen.Clear();
+            for (int imat = 0; imat < nbrMatieres; imat++)
+            {
+                listMoyenneGen.Add(moyenneMatiere(imat));
+            }
+            moyenne = listMoyenneGen.Average();
+			moyenne = (float)Math.Round(moyenne, 2);
+            return moyenne;
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMoyennes
+{
+    class Eleve
+    {
+        //Déclaration des variables
+        public string prenom { get; private set; }
+        public string nom { get; private set; }
+        public int numero { get; private set; }
+        public static List<List<List<float>>> notes { get; private set; } = new List<List<List<float>>>(); // notes de tous les élèves dans toutes les matières
+        public List<List<float>> notesEleve { get; private set; } = new List<List<float>>();               // notes d'un élève dans toutes les matières
+        public List<float> notesMatiere { get; private set; } = new List<float>();                         // notes d'un élève dans une matière
+        public int k1 { get; private set; } // compteur pour le remplissage du tableau des notes
+        public int k2 { get; private set; } // compteur pour le remplissage du tableau des notes
+        public float moyenne { get; private set; }
+        public List<float> listMoyenne { get; private set; } = new List<float>();
+
+        // Constructeur
+        public Eleve(string aPrenomEleve, string aNomEleve, int aNumEleve)
+        {
+            prenom = aPrenomEleve;
+            nom = aNomEleve;
+            numero = aNumEleve;
+            k1 = 0;
+            k2 = 0;
+        }
+        
+        // Remplissage du tableau des notes des élèves
+        public void ajouterNote(Note n)
+        {
+            notesMatiere.Add(n.note);
+            k1++;
+            if (k1 == 5)
+            {
+                notesEleve.Add(notesMatiere.ToList());
+                k1 = 0; 
+                notesMatiere.Clear();
+                k2++;
+            }
+            if (k2 == 4)
+            {
+                notes.Add(notesEleve.ToList());
+                k2 = 0;
+                notesEleve.Clear();
+            }
+        }
+
+        // Calcul de la moyenne d'un élève une matière
+        public float moyenneMatiere(int numMatiere)
+        {
+            moyenne = notes[numero][numMatiere].Average();
+            moyenne = (float)Math.Round(moyenne, 2);
+            return moyenne;
+        }
+
+        // Calcul de la moyenne générale d'un élève
+        public float moyenneGeneral()
+        {
+            listMoyenne.Clear();
+            for (int i= 0; i < 4; i++) {
+                listMoyenne.Add(moyenneMatiere(i));
+            }
+            moyenne = listMoyenne.Average();
+            moyenne =(float)Math.Round(moyenne, 2);
+            return moyenne;
+        }
+    }
+}
